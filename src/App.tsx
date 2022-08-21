62f9b267e37e52713b7831a91bab0a09aa4a45c5
xXLXx <leo.lavarias@gmail.com>
2022-08-22 00:22:20 +0800

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import SportsTennis from '@mui/icons-material/SportsTennis';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Bracket from './Bracket';
+import Profiles from './Profiles';
 import './App.css';
 
 function App() {
+  const [value, setValue] = React.useState('bracket');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ pb: 7 }} >
+        { value === 'profiles' ? <Profiles /> : null }
+        { value === 'bracket' ? <Bracket /> : null }
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction value="profiles" label="Profiles" icon={<AccountCircle />} />
+            <BottomNavigationAction value="bracket" label="Bracket" icon={<SportsTennis />} />
+          </BottomNavigation>
+        </Paper>
+      </Box>
     </div>
   );
 }
---
+++ src/Bracket.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from '@mui/material/List';
+
+function Bracket() {
+
+  return (
+    <>
+      <p>Bracket</p>
+      <List />
+    </>
+  );
+}
+
+export default Bracket;
